087d1bb76b65d31164697d22f01a065ecce624cc
GianfrancoCh <gianfrancochiarizia@gmail.com>
2025-05-30 21:24:02 -0300

    Eliminar carpeta environments del repositorio y agregar al .gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://pdrmqdvytobcrhfdmolr.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkcm1xZHZ5dG9iY3JoZmRtb2xyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NjYwOTgsImV4cCI6MjA2NDE0MjA5OH0.ZJm-FD-bzWOGggu-i8LWJmeB34bJ44vhTSBYE3mjLZs',
-};
