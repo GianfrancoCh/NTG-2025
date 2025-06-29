897214b2663dfdcf0e7ef8379dc7604029633954
nahuel <nahuel.borquez@yahoo.com>
2025-06-29 16:52:33 -0300

    lista pedidos pendientes

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -118,7 +118,7 @@ export class DatabaseService {
     const { error } = await this.supabase
       .from(coleccion)
       .update(data)
-      .eq('id', docId);
+      .eq('id', Number(docId));
 
     if (error) {
       console.error('Error al actualizar documento:', error.message);
